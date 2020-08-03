5c9f4010bcd8f585f16d7a657a410392d69ba9d0
Aristana Scourtas <ascourtas@gmail.com>
2020-08-03 14:06:46 -0500

    add models package and original CNN code as module

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,2 @@
 from . import utils
+from .models import original_cnn
---
+++ src/models/original_cnn.py
@@ -0,0 +1,68 @@
+"""
+original_cnn.py
+
+The CNN model developed by UCSD researchers for smoke detection. Currently a WIP.
+
+Last updated from original repo: Feb/March 2020
+Original repo: https://gitlab.nautilus.optiputer.net/ar-noc/keras-smoke-detection/-/tree/master
+"""
+
+import tensorflow as tf; print(tf.__version__)
+from keras import backend as K
+K.tensorflow_backend._get_available_gpus()
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.layers import SeparableConv2D
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import Activation
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import Dense
+
+
+class FireDetectionNet:
+    @staticmethod
+    def build(width, height, depth, classes):
+        # initialize the model along with the input shape to be
+        # "channels last" and the channels dimension itself
+        model = Sequential()
+        inputShape = (height, width, depth)
+        chanDim = -1
+
+        model.add(SeparableConv2D(16, (7, 7), padding="same",
+                                  input_shape=inputShape))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+
+        model.add(SeparableConv2D(32, (3, 3), padding="same"))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+
+        model.add(SeparableConv2D(64, (3, 3), padding="same"))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(SeparableConv2D(64, (3, 3), padding="same"))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+
+        model.add(Flatten())
+        model.add(Dense(128))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.5))
+
+        # second set of FC => RELU layers
+        model.add(Dense(128))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.5))
+
+        # softmax classifier
+        model.add(Dense(classes))
+        model.add(Activation("softmax"))
+
+        # return the constructed network architecture
+        return model
